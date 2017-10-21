28bd66e9a39a4d8619fc59b64648b4670366c4d7
Markus <mmattes87@gmail.com>
2017-10-21 18:19:43 +0200

    test
    Signed-off-by: Markus <mmattes87@gmail.com>

--- Build/deploy.ps1
+++ Build/deploy.ps1
@@ -1,5 +1,1 @@
-dir$PSScriptRoot
-dir $PSScriptRoot\..\Source
-dir $ENV:APPVEYOR_BUILD_FOLDER
-dir $ENV:APPVEYOR_BUILD_FOLDER\Source
-Publish-Module -Path $ENV:APPVEYOR_BUILD_FOLDER\Invoke-Install\Source\Invoke-Install.psd1 -NuGetApiKey $ENV:NuGetApiKey -Verbose
+Publish-Module -Path "$ENV:APPVEYOR_BUILD_FOLDER\Source\Invoke-Install.psd1" -NuGetApiKey $ENV:NuGetApiKey -Verbose
