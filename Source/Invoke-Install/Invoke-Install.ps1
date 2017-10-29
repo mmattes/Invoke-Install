e7257c9b40be7853d768a8f42b4a1348a3a62807
Markus <mmattes87@gmail.com>
2017-10-29 15:17:59 +0100

    added description to functions

--- Source/Invoke-Install/Invoke-Install.ps1
+++ Source/Invoke-Install/Invoke-Install.ps1
@@ -1,15 +1,32 @@
-# TODO: Propper Description of this file above
-# TODO: Tests? 
-
 function Invoke-Install (
+     <#
+        .SYNOPSIS
+            Gets all *.install.ps1 and executes them
+        
+        .DESCRIPTION
+            Gets all *.install.ps1 files and will execute them one by one
+
+        .PARAMETER Path
+            Path to the *.install.ps1 file/files
+
+        .PARAMETER Filter
+            Filter for which scripts Invoke-Install should look for to execute them
+
+        .EXAMPLE
+            Invoke-Install HelloWorld.install.ps1
+
+        .EXAMPLE
+            Invoke-Install . -Filter ".*\.mynameing\.ps1"
+    #>
+
     [Parameter(Position=0, Mandatory=$true, HelpMessage="Path to the *.install.ps1")]
-    [string] $InstallFilePath,
+    [string] $Path,
 
     [string] $Filter =  ".*\.install\.ps1"
 ) {
-    $InstallScripts = Get-InstallScripts -InstallFilePath $InstallFilePath -Filter $Filter
+    $InstallScripts = Get-InstallScripts -Path $Path -Filter $Filter
 
-    $Count = 0 
+    $Count = 0
 
     foreach ($Script in $InstallScripts) {
         . $Script
@@ -20,23 +37,42 @@ function Invoke-Install (
 }
 
 function Get-InstallScripts (
+         <#
+        .SYNOPSIS
+            Gets all scripts within a specific folder which match the filter
+        
+        .DESCRIPTION
+            Gets all scripts within a specific folder which match the filter
+
+        .PARAMETER Path
+            Path to the files
+
+        .PARAMETER Filter
+            Filter for which scripts should be looked for
+
+        .EXAMPLE
+            Get-InstallScripts .
+
+        .EXAMPLE
+            Get-InstallScripts . -Filter ".*\.mynameing\.ps1"
+    #>
     [Parameter(Position=0, Mandatory=$true, HelpMessage="Path to the *.install.ps1")]
-    [string] $InstallFilePath, 
+    [string] $Path, 
 
     [string] $Filter
 ) {
     $InstallScripts = @()
     
-    if ((Get-Item $InstallFilePath) -is [System.IO.DirectoryInfo]) {
-        $FoundInstallScripts = Get-ChildItem $InstallFilePath | Where-Object {$_.Name -match  $Filter }
+    if ((Get-Item $Path) -is [System.IO.DirectoryInfo]) {
+        $FoundInstallScripts = Get-ChildItem $Path | Where-Object {$_.Name -match  $Filter }
         if ($FoundInstallScripts.Length -gt 0) {            
             $FoundInstallScripts | ForEach-Object {
                 $InstallScripts += $_.FullName
             }
         }
     } else {
-        if ($InstallFilePath -match $Filter) {
-            $InstallScripts += (Get-Item $InstallFilePath)    
+        if ($Path -match $Filter) {
+            $InstallScripts += (Get-Item $Path)    
         }        
     }
     
