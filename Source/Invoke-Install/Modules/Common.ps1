c957d9c23eb88263252d3c917f69e46eca2a236a
Markus Mattes <M.Mattes.TUTzuH2008@vetz.de>
2018-01-26 21:17:02 +0100

    Added Get-FreePortFromRange

--- Source/Invoke-Install/Modules/Common.ps1
+++ Source/Invoke-Install/Modules/Common.ps1
@@ -37,4 +37,65 @@ function Get-ShortGUID {
     [regex]::Replace([System.Convert]::ToBase64String([guid]::NewGuid().toByteArray()), "[/+=]", $RandomChar)
 }
 
-#TODO: Get free port from range windows
+
+function Get-FreePortFromRange
+{
+    <#
+        .SYNOPSIS
+            Provides the next free port from a range
+        
+        .DESCRIPTION
+            Provides the next free port from a range of port numbers passed to the function
+
+        .PARAMETER PortRange
+            Specifies the range of ports which should be checked for a not used port
+
+        .PARAMETER IpAddress
+            Specifies the IpAddress which should be checked for a not used port from the range specified
+        
+        .EXAMPLE
+            Get-FreePortFromRange -PortRange (8000..8100)
+
+        .EXAMPLE
+            Get-FreePortFromRange -PortRange (8000..8100) -IpAddress "192.168.1.20"
+    #>
+    param(
+        [Parameter(Mandatory=$true, Position=1)]
+        [array]$PortRange  = $null,
+        
+        [Parameter(Mandatory=$false, Position=2)]
+        [string]$IpAddress = $null
+    )
+
+    Begin {
+        if (!(Get-Module -ListAvailable -Name Nettcpip)) {
+            throw "Nettcpip Module not available"
+        } 
+
+        if (!(Get-Module Nettcpip))
+        {
+            Import-Module Nettcpip -ErrorAction Stop            
+        }
+    }
+    
+    Process {
+        $PortsInUse = @()
+        if($IpAddress) {
+            Get-NetTCPConnection -State "Listen" -LocalAddress $IpAddress | ForEach-Object { $PortsInUse += $_.LocalPort }
+        } else {
+            Get-NetTCPConnection -State "Listen" | ForEach-Object { $PortsInUse += $_.LocalPort }
+        }
+        
+        
+        $PortRange | ForEach-Object {           
+            if ($PortsInUse -notcontains $_) {
+                return $_
+            }
+        }
+        return $null
+    }
+    
+    End { 
+    }
+}
+
