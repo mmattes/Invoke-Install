4b1f3512ac7406ab5775d77db9e161334cbe922b
Markus Mattes <m.mattes@vetz.vet>
2019-12-20 17:39:29 +0100

    added chocolatey deployment to appveyor

---
+++ Build/deploy_chocolatey.ps1
@@ -0,0 +1,30 @@
+# AU Packages Template: https://github.com/majkinetor/au-packages-template
+
+param([string] $Name, [string] $ForcedPackages, [string] $Root = "$PSScriptRoot\Chocolatey")
+
+if (Test-Path $PSScriptRoot/update_vars.ps1) { . $PSScriptRoot/update_vars.ps1 }
+
+$Options = [ordered]@{
+    Timeout        = 100                                     #Connection timeout in seconds
+    UpdateTimeout  = 1200                                    #Update timeout in seconds
+    Threads        = 10                                      #Number of background jobs to use
+    Push           = $Env:au_Push -eq 'true'                 #Push to chocolatey
+    PluginPath     = ''                                      #Path to user plugins
+
+    ForcedPackages = $ForcedPackages -split ' '
+    BeforeEach     = {
+        param($PackageName, $Options )
+        $p = $Options.ForcedPackages | ? { $_ -match "^${PackageName}(?:\:(.+))*$" }
+        if (!$p) { return }
+
+        $global:au_Force = $true
+        $global:au_Version = ($p -split ':')[1]
+    }
+}
+
+if ($ForcedPackages) { Write-Host "FORCED PACKAGES: $ForcedPackages" }
+$global:au_Root = $Root                                    #Path to the AU packages
+$global:info = updateall -Name $Name -Options $Options
+
+#Uncomment to fail the build on AppVeyor on any package error
+#if ($global:info.error_count.total) { throw 'Errors during update' }
--- Chocolatey/Invoke-Install/update.ps1
+++ Chocolatey/Invoke-Install/update.ps1
@@ -40,4 +40,4 @@ function global:au_GetLatest {
     }
 }
 
-update -ChecksumFor none
+Update-Package -ChecksumFor none
